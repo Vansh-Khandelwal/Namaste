2e10fce14e014cd5bd076e96c4f45ae25123e12d
Vansh <vansh.khandelwal06@gmail.com>
2022-08-07 16:36:44 +0530

    Second Commit

---
+++ namaste/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Namaste
+      <div className="circle" style={{top:'-18%', right:'0'}}></div>
+      <div className="circle" style={{top:'36%', left:'-8rem'}}></div>
+    </div>
+  );
+}
+
+export default App;
