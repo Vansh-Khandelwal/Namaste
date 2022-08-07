9fec5d73ed983bcde4f77ee32c1f34e6afb314d1
Vansh <vansh.khandelwal06@gmail.com>
2022-08-07 17:04:09 +0530

    Fivth Commit

---
+++ namaste/src/Components/Profile/Profile-Card/Profile_card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile_card = () => {
+  return (
+    <div>Profile_card</div>
+  )
+}
+
+export default Profile_card
--- namaste/src/Components/Profile/Profile.jsx
+++ namaste/src/Components/Profile/Profile.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+
+import Following from './Following/Following'
+import Profile_card from './Profile-Card/Profile_card'
 import Search from './Search/Search'
 
 const Profile = () => {
   return (
     <div className='profile'>
       <Search/>
+      <Profile_card/>
+      <Following/>
     </div>
   )
 }
