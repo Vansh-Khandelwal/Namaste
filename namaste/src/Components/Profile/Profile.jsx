271fec4f912fe873c6785c107e999b898e4d9d42
Vansh <vansh.khandelwal06@gmail.com>
2022-08-07 17:22:39 +0530

    Sixth Commit

--- namaste/src/Components/Profile/Profile.jsx
+++ namaste/src/Components/Profile/Profile.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 
 import Following from './Following/Following'
-import Profile_card from './Profile-Card/Profile_card'
+import ProfileCard from './Profile_Card/Profile_card'
 import Search from './Search/Search'
 
 const Profile = () => {
   return (
     <div className='profile'>
       <Search/>
-      <Profile_card/>
+      <ProfileCard/>
       <Following/>
     </div>
   )
