2977ef1a7d7a2e7a8bee4e67a578b3da80b50127
Vansh <vansh.khandelwal06@gmail.com>
2022-08-10 01:05:30 +0530

    Twelveth Commit

--- namaste/src/Components/Profile/Profile_Card/Profile_card.jsx
+++ namaste/src/Components/Profile/Profile_Card/Profile_card.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Profile_card.css'
 
+import cover from '../../../images/cover.jpg'
+import dp from '../../../images/dp.jpg'
+
 const Profile_card = () => {
   return (
-    <div></div>
+    <div className='profile-card'>
+      <div className="profile-images">
+        <img src={cover} alt="" className='cover'/>
+        <img src={dp} alt="" className='dp'/>
+      </div>
+    </div>
   )
 }
 
