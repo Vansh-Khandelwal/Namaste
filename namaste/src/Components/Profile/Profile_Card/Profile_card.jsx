d6a0af6274de3256896151f8b1c4e92d315cfabb
Vansh <vansh.khandelwal06@gmail.com>
2022-08-10 18:35:46 +0530

    Thirteenth Commit

--- namaste/src/Components/Profile/Profile_Card/Profile_card.jsx
+++ namaste/src/Components/Profile/Profile_Card/Profile_card.jsx
@@ -8,8 +8,34 @@ const Profile_card = () => {
   return (
     <div className='profile-card'>
       <div className="profile-images">
-        <img src={cover} alt="" className='cover'/>
-        <img src={dp} alt="" className='dp'/>
+        <img src={cover} alt="cover_image"/>
+        <img src={dp} alt="display_image"/>
+      </div>
+
+      <div className="profile-about">
+        <div className="profile-name">Vansh Khandelwal</div>
+        <div className="profile-description">Full Stack Web Developer</div>
+      </div>
+
+      <div className="follow-status">
+        <hr />
+        <div className='f-status'>
+          <div className="followers">
+            <span className="num-followers">6,235</span>
+            <span>Followers</span>
+          </div>
+          <div className="vertical"></div>
+          <div className="followings">
+            <span className="num-following">1223</span>
+            <span>Following</span>
+          </div>         
+          <div className="vertical"></div>
+          <div className="posts">
+            <span className="num-posts">3</span>
+            <span>Posts</span>
+          </div>
+        </div>
+        <hr />
       </div>
     </div>
   )
