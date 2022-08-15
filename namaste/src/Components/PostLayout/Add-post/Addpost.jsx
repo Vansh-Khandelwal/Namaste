ddc9723d0f3b273cea32d103ebcd4ba4e71519f5
Vansh <vansh.khandelwal06@gmail.com>
2022-08-15 17:14:19 +0530

    Nineteenth Commit

--- namaste/src/Components/PostLayout/Add-post/Addpost.jsx
+++ namaste/src/Components/PostLayout/Add-post/Addpost.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import './Addpost.css'
 
 import dp from '../../../images/dp.jpg'
 
-import {MdImage, MdVideocam, MdLocationPin, MdSchedule} from 'react-icons/md'
+import {MdImage, MdVideocam, MdLocationPin, MdSchedule, MdOutlineCancel} from 'react-icons/md'
 
 export const Addpost = () => {
+
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+
+  const onImageChange = (event)=>{
+    if(event.target.files&&event.target.files[0])
+    {
+      let img = event.target.files[0];
+      setImage({image: URL.createObjectURL(img)},);
+    }
+  }
+
   return (
     <div className="Addpost">
         <div className="addpost-input">
@@ -13,12 +25,22 @@ export const Addpost = () => {
             <input type="text" className="addpost-caption" placeholder="What's happening?"/>
         </div>
         <div className="addpost-options">
-            <div className="i image-icon"><MdImage/><span>Photos</span></div>
+            <div className="i image-icon" onClick={()=>imageRef.current.click()}><MdImage/><span>Photos</span></div>
             <div className="i video-icon"><MdVideocam/><span>Video</span></div>
             <div className="i location-icon"><MdLocationPin/><span>Location</span></div>
             <div className="i schedule-icon"><MdSchedule/><span>Schedule</span></div>
             <button className="button">Share</button>
+            <div style={{display: "none"}}>
+              <input type="file" name="MyImage" ref={imageRef} onChange={onImageChange}/>
+            </div>
         </div>
+        {image && 
+          // above statement means if image exist then(represented by &&)
+          <div className="previewImage">
+            <MdOutlineCancel onClick={()=>setImage(null)}/>
+            <img src={image.image} alt="" />
+          </div>
+        }
     </div>
   )
 }
