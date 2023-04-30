67ebe913d89329e49800f78516c26520900c5e9d
Vansh <vansh.khandelwal06@gmail.com>
2023-04-30 17:21:51 +0530

    Host Commit

--- namaste/src/Api/AuthRequest.js
+++ namaste/src/Api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: process.env.BASE_URL })
+const API = axios.create({ baseURL: "https://namaste-yzsb.onrender.com" })
 
 export const login = (formData) => API.post('/auth/login', formData)
 export const signup = (formData) => API.post('/auth/register', formData)
