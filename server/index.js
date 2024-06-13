688a267649c4251c89b1080d71a074bb93524add
Vansh <vansh.khandelwal06@gmail.com>
2024-06-13 18:49:50 +0530

    CORS Deploy

--- server/index.js
+++ server/index.js
@@ -17,10 +17,7 @@ const app = express();
 
 // CORS configuration
 // For Cors Error
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}));
+app.use(cors());
 
 // To serve images for public
 
