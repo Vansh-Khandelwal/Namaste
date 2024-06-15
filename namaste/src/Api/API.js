c76e55be2edcf94c5b2e9170cefa1c465daf1df8
Vansh <vansh.khandelwal06@gmail.com>
2024-06-15 13:53:18 +0530

    CORS Deploy

--- namaste/src/Api/API.js
+++ namaste/src/Api/API.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export const API = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
-    // headers: {
-    //     "Access-Control-Allow-Origin": "*"
-    //         // "Content-Type": "application/json",
-    // }
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+            // "Content-Type": "application/json",
+    }
 })
